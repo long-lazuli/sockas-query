99cc621a5e66fabe9840cbf53a457e25e4c4faa7
Long Lazuli <long.lazuli@gmail.com>
2026-04-06 11:35:35 +0200

    chore: add @tanstack/eslint-config and @cspell/eslint-plugin to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,33 @@
 // @ts-check
 
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
+// @ts-ignore Needed due to moduleResolution Node vs Bundler
+import { tanstackConfig } from '@tanstack/eslint-config'
+import pluginCspell from '@cspell/eslint-plugin'
 import reactHooks from 'eslint-plugin-react-hooks'
 
-export default tseslint.config(
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
+export default [
+  ...tanstackConfig,
   {
+    name: 'react-sockas/main',
     plugins: {
+      cspell: pluginCspell,
       'react-hooks': reactHooks,
     },
     rules: {
+      'cspell/spellchecker': [
+        'warn',
+        {
+          cspell: {
+            words: [
+              'sockas', // Our package name
+              'tanstack', // Our peer dependency scope
+              'refetches', // Query refetch operations
+              'tsup', // We use tsup as builder
+              'typecheck', // Field of vite.config.ts
+            ],
+          },
+        },
+      ],
       '@typescript-eslint/no-empty-function': 'off',
       '@typescript-eslint/no-unsafe-function-type': 'off',
       'no-case-declarations': 'off',
@@ -19,4 +35,4 @@ export default tseslint.config(
       'react-hooks/exhaustive-deps': 'warn',
     },
   },
-)
+]
